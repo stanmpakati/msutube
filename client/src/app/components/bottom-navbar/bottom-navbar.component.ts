06635193128df3580b3f60049a6684f4aee412b5
stan000 <stanmpakatig@gmail.com>
2021-07-20 15:03:02 +0200

    more dynamic bottom navbar

--- client/src/app/components/bottom-navbar/bottom-navbar.component.ts
+++ client/src/app/components/bottom-navbar/bottom-navbar.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ThemeService } from 'src/app/_services/theme.service';
 
 @Component({
   selector: 'app-bottom-navbar',
   templateUrl: './bottom-navbar.component.html',
-  styleUrls: ['./bottom-navbar.component.scss']
+  styleUrls: ['./bottom-navbar.component.scss'],
 })
-export class BottomNavbarComponent implements OnInit {
+export class BottomNavbarComponent implements OnInit, OnDestroy {
+  className!: string;
+  isDarkmode!: boolean;
+  themeSub: Subscription = new Subscription();
 
-  constructor() { }
+  constructor(private themeService: ThemeService) {}
 
   ngOnInit(): void {
+    this.themeSub = this.themeService.themeStatusListener.subscribe(
+      (isDark) => {
+        this.isDarkmode = isDark;
+        // Todo: make it refresh active color
+        isDark ? (this.className = 'active-dark') : (this.className = 'active');
+      }
+    );
+
+    this.themeService.getTheme();
   }
 
+  ngOnDestroy(): void {
+    this.themeSub.unsubscribe();
+  }
 }

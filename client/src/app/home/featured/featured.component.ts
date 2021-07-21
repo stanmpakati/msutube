4f80d6c124ad0357a2fd6c750b5a8e96208fe0f1
stan000 <stanmpakatig@gmail.com>
2021-07-21 11:08:57 +0200

    added fake thumbnails

--- client/src/app/home/featured/featured.component.ts
+++ client/src/app/home/featured/featured.component.ts
@@ -27,6 +27,48 @@ export class FeaturedComponent implements OnInit {
         thumbnailUrl: '',
         uploadDate: new Date(),
       },
+      {
+        title: 'My Third Fake Movie',
+        length: '1.04:12',
+        owner: 'Various Owners',
+        thumbnailUrl: '',
+        uploadDate: new Date('December 20, 2020 16:24:00'),
+      },
+      {
+        title: 'My fourth Fake Video',
+        length: '19:52',
+        owner: 'stanmpakati',
+        thumbnailUrl: '',
+        uploadDate: new Date(),
+      },
+      {
+        title: 'My Fifth Fake Movie',
+        length: '1.04:12',
+        owner: 'Various Owners',
+        thumbnailUrl: '',
+        uploadDate: new Date('July 20, 2021 16:24:00'),
+      },
+      {
+        title: 'My Sixth Fake Video',
+        length: '03:22',
+        owner: 'ganizanimpakati',
+        thumbnailUrl: '',
+        uploadDate: new Date(),
+      },
+      {
+        title: 'My Sevent Fake Movie',
+        length: '1.04:12',
+        owner: 'Various Owners',
+        thumbnailUrl: '',
+        uploadDate: new Date('July 21, 2021 06:24:00'),
+      },
+      {
+        title: 'My Last Fake Video',
+        length: '09:52',
+        owner: 'stanmpakati',
+        thumbnailUrl: '',
+        uploadDate: new Date(),
+      },
     ];
   }
 }

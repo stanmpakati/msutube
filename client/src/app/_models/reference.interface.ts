4cf83bbb2b59280ca5846ffb4b943f00f3ecf1a3
stan000 <stanmpakatig@gmail.com>
2021-07-23 16:05:53 +0200

    references component logic done

---
+++ client/src/app/_models/reference.interface.ts
@@ -0,0 +1,11 @@
+export interface Reference {
+  author?: string;
+  publicationDate?: Date;
+  title?: string;
+  edition?: string;
+  place?: string;
+  publisher?: string;
+  book?: string;
+  link?: string;
+  dateAccessed?: Date;
+}
--- client/src/app/upload/references-form/references-form.component.ts
+++ client/src/app/upload/references-form/references-form.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { DateAdapter } from '@angular/material/core';
+import { MatDatepickerInputEvent } from '@angular/material/datepicker';
+import { Reference } from 'src/app/_models/reference.interface';
 
 @Component({
   selector: 'app-references-form',
@@ -8,31 +11,55 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class ReferencesFormComponent implements OnInit {
   refForm!: FormGroup;
+  references: Reference[] = [];
+  today = new Date();
+  min!: Date | null;
 
-  constructor() {}
+  constructor(private dateAdapter: DateAdapter<Date>) {
+    this.dateAdapter.setLocale('en-GB');
+  }
 
   ngOnInit(): void {
     const urlRegex =
       /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/;
     this.refForm = new FormGroup({
       author: new FormControl(null, {
-        validators: [Validators.required, Validators.pattern(urlRegex)],
-        updateOn: 'blur',
+        validators: [],
+        updateOn: 'submit',
       }),
       publicationDate: new FormControl(null, {
-        validators: [Validators.required, Validators.pattern(urlRegex)],
-        updateOn: 'blur',
+        validators: [Validators.required],
+        updateOn: 'submit',
       }),
       dateAccessed: new FormControl(null, {
-        validators: [Validators.required, Validators.pattern(urlRegex)],
-        updateOn: 'blur',
+        validators: [Validators.required],
+        updateOn: 'submit',
       }),
       title: new FormControl(null),
       book: new FormControl(null),
       link: new FormControl(null, {
         validators: [Validators.required, Validators.pattern(urlRegex)],
-        updateOn: 'blur',
+        updateOn: 'submit',
       }),
     });
+  }
+
+  choosePublicationDate(event: MatDatepickerInputEvent<Date>) {
+    this.min = event.value;
+  }
+
+  addReference() {
+    const ref: Reference = {
+      author: this.refForm.value.author as string,
+      title: this.refForm.value.title as string,
+      publicationDate: this.refForm.value.publicationDate as Date,
+      dateAccessed: this.refForm.value.dateAccessed as Date,
+      link: this.refForm.value.link as string,
+    };
+    console.log(ref);
+    this.references.push(ref);
+
+    this.refForm.reset();
+    console.log(this.references);
   }
 }

e90745ad66ee3fcf6e7bd4b97b13928d80e39ba5
stan000 <stanmpakatig@gmail.com>
2021-08-11 11:34:56 +0200

    created owner components

--- client/src/app/components/owner-component/owner-component.component.ts
+++ client/src/app/components/owner-component/owner-component.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/_services/user.service';
 
 @Component({
   selector: 'app-owner-component',
   templateUrl: './owner-component.component.html',
-  styleUrls: ['./owner-component.component.scss']
+  styleUrls: ['./owner-component.component.scss'],
 })
 export class OwnerComponentComponent implements OnInit {
+  @Input() username!: string;
+  userDetails!: {
+    first: string;
+    last: string;
+    username: string;
+    profilePicUrl: string | null | undefined;
+  };
 
-  constructor() { }
+  constructor(private userService: UserService, private router: Router) {}
 
   ngOnInit(): void {
+    // Get user details from backend
+    this.userService.getUser(this.username).subscribe((userData) => {
+      const user = userData.user;
+      this.userDetails = {
+        ...user.name,
+        username: this.username,
+        profilePicUrl: user.profilePicUrl,
+      };
+    });
   }
 
+  navigateToProfile() {
+    this.router.navigateByUrl(`profile/${this.username}`);
+  }
 }

c236960fd6b712108ea3fc5e98d8bd95ebf230e0
stan000 <stanmpakatig@gmail.com>
2021-08-12 13:01:08 +0200

    changing thumbnail based on theme

--- client/src/app/components/thumbnail/thumbnail.component.ts
+++ client/src/app/components/thumbnail/thumbnail.component.ts
@@ -1,6 +1,8 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { Thumbnail } from 'src/app/_models/thumbnail';
+import { ThemeService } from 'src/app/_services/theme.service';
 import { timeAgo } from 'src/app/_services/time-ago.service';
 
 @Component({
@@ -8,20 +10,37 @@ import { timeAgo } from 'src/app/_services/time-ago.service';
   templateUrl: './thumbnail.component.html',
   styleUrls: ['./thumbnail.component.scss'],
 })
-export class ThumbnailComponent implements OnInit {
+export class ThumbnailComponent implements OnInit, OnDestroy {
   @Input() thumbnailData!: Thumbnail;
   timeElapsed!: string;
   owner!: string;
+  themeSub: Subscription = new Subscription();
+  isDarkMode!: boolean;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private themeService: ThemeService) {}
 
   ngOnInit(): void {
+    // Calculate time submited relative to today
     this.timeElapsed = timeAgo(this.thumbnailData.createdAt);
+
+    // Determine how many authors there are
     if (this.thumbnailData.owners.length > 1) this.owner = 'Various Owners';
     else this.owner = this.thumbnailData.owners[0];
+
+    // To register theme changes
+    this.themeSub = this.themeService.themeStatusListener.subscribe(
+      (isDark) => {
+        this.isDarkMode = isDark;
+      }
+    );
+    this.themeService.getTheme();
   }
 
   navigateToPage() {
     this.router.navigate(['/video', this.thumbnailData._id]);
+  }
+
+  ngOnDestroy() {
+    this.themeSub.unsubscribe();
   }
 }

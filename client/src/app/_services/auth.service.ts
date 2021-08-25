b4ac8a6a991c8c039b01e38953bd41c3a61ee1ed
stan000 <stanmpakatig@gmail.com>
2021-08-25 13:52:03 +0200

    I have no Idea

--- client/src/app/_services/auth.service.ts
+++ client/src/app/_services/auth.service.ts
@@ -155,7 +155,7 @@ export class AuthService {
               return;
             }
             // Otherwise go to homepage
-            this.router.navigateByUrl('/upload');
+            this.router.navigateByUrl('/home');
           }
         },
         (error) => {

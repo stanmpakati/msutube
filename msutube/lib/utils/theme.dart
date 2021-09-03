de53f2d5ff5492c6c15e9878ec4bf347dfe869af
stan000 <stanmpakatig@gmail.com>
2021-09-03 08:42:29 +0200

    set up theme

---
+++ msutube/lib/utils/theme.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+ThemeData lightTheme = ThemeData(
+  // Colors
+  brightness: Brightness.light,
+  primarySwatch: Colors.blue,
+  accentColor: Colors.amber[200],
+);
+
+ThemeData darkTheme = ThemeData(
+  // Colors
+  brightness: Brightness.dark,
+  primaryColor: Colors.blue[300],
+  accentColor: Colors.amber[200],
+);
